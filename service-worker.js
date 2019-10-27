8ce0fbd080ec34f9aaec759ee30667634be97d16
Hong <henrylei80@yahoo.com>
2019-10-27 15:41:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-context-api/precache-manifest.881576647a3fc71f77d552acef5d8049.js"
+  "/react-context-api/precache-manifest.35736533726712c0b21fa0d9402f154d.js"
 );
 
 workbox.clientsClaim();
